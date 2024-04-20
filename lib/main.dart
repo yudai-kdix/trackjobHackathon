63a6a41d9596bf89018cca64bfdf8264f12ca6cd
yudai-kdix <yudai.homma222@gmail.com>
2024-04-20 14:47:13 +0900

    モデルと単語一覧を仮置き

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trackjob2024/views/question_list_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,27 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+      title: '単語暗記アプリ',
+      theme: ThemeData( // ここでテーマを設定
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: { //ここで定義したものは他で Navigator.pushNamed(context, '/ルート名');で画面が遷移するはず
+        '/': (context) => const MyHomePage(title: '単語暗記アプリ'),
+        // TODO: クラスを設定したら外す。
+        // '/settings': (context) => SettingsView(), // 設定画面
+        '/word_list': (context) => QuestionListView(), // 単語一覧画面
+        // '/word_add':(context) => WordAddView(), // 単語追加画面
+        // '/word_answer': (context) => WordAnswerView(), // 単語回答画面
+        // 'word_detail': (context) => WordDetailView(), // 単語詳細画面
+        
+      },
     );
   }
 }
