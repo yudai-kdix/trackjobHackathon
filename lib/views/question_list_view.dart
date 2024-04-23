cd147ccb5535ac7870ddb5a6794358f4fc9ec29b
yuto141592 <aricchi0208@icloud.com>
2024-04-23 23:04:48 +0900

    start

--- lib/views/question_list_view.dart
+++ lib/views/question_list_view.dart
@@ -30,8 +30,8 @@ class _QuestionListViewState extends State<QuestionListView> {
           return Card(
             color: Colors.grey[200],
             child: ListTile(
-              title: Text(word.term),
-              subtitle: Text(word.definition),
+            title: Text(word.term),
+            subtitle: Text(word.definition),
               trailing: Wrap(
                 spacing: 8, // アイコンの間の幅を調整
                 children: [
@@ -63,7 +63,7 @@ class _QuestionListViewState extends State<QuestionListView> {
                   ),
                 ],
               ),
-              onTap: () =>
+            onTap: () =>
                 //Navigator.pushNamed(context, '/detail', arguments: word),
                 Navigator.pushNamed(context, '/word_answer'),
             ),
